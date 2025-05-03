a7e0ba7fa3e616c27ebb8333fd704acf5fd0aed9
arturbaccarin <baccarin.artur@gmail.com>
2025-05-03 14:10:25 -0300

    feat: add more about tasks

--- task-tracker/task.go
+++ task-tracker/task.go
@@ -1,6 +1,6 @@
 package main
 
-type Tasks map[string]Task
+import "fmt"
 
 type status string
 
@@ -10,10 +10,38 @@ const (
 	Done       status = "done"
 )
 
+type Tasks map[string]Task
+
+func (t Tasks) Add(task *Task) {
+	t[task.Title] = *task
+}
+
+func (t Tasks) Remove(title string) {
+	delete(t, title)
+}
+
+func (t Tasks) ListAll() {
+	for _, task := range t {
+		fmt.Printf("%s\n", task)
+	}
+}
+
+func (t Tasks) ListByStatus(status status) {
+	for _, task := range t {
+		if task.Status == status {
+			fmt.Printf("%s\n", task)
+		}
+	}
+}
+
 type Task struct {
 	Title  string
 	Body   string
 	Status status
+}
+
+func (t Task) String() string {
+	return fmt.Sprintf("Title: %s\nBody: %s\nStatus: %s\n", t.Title, t.Body, t.Status)
 }
 
 func NewTask(title, body string) Task {
@@ -26,4 +54,12 @@ func NewTask(title, body string) Task {
 
 func (t *Task) MarkAsDone() {
 	t.Status = Done
+}
+
+func (t *Task) MarkAsInProgress() {
+	t.Status = InProgress
+}
+
+func (t *Task) MarkAsOpen() {
+	t.Status = Open
 }
