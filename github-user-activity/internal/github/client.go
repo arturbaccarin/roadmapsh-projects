df22cc1bcdd4c682e98148e90681ee88f96fbf8e
arturbaccarin <baccarin.artur@gmail.com>
2025-06-01 11:09:35 -0300

    feat: start client

--- github-user-activity/internal/github/client.go
+++ github-user-activity/internal/github/client.go
@@ -1,1 +1,13 @@
 package github
+
+import "net/http"
+
+func NewClient(request *http.Request) *Client {
+	return &Client{
+		request: request,
+	}
+}
+
+func (c *Client) GetListEventsUser(username string) ([]UserEvent, error) {
+	return nil, nil
+}
