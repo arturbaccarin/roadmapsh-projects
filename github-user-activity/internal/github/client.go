e2dc00a82fc86de5617b3f763ef526f34b67e0d5
arturbaccarin <baccarin.artur@gmail.com>
2025-06-04 07:26:40 -0300

    feat: finish GetListEventsUser

--- github-user-activity/internal/github/client.go
+++ github-user-activity/internal/github/client.go
@@ -1,13 +1,53 @@
 package github
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"githubuseractivity/config"
+	"githubuseractivity/internal/requester"
+	"io"
+)
 
-func NewClient(request *http.Request) *Client {
+const (
+	hostname = "https://api.github.com"
+)
+
+type Client struct {
+	requester requester.Requester
+}
+
+func NewClient(requester requester.Requester) *Client {
 	return &Client{
-		request: request,
+		requester: requester,
 	}
 }
 
 func (c *Client) GetListEventsUser(username string) ([]UserEvent, error) {
-	return nil, nil
+	url := fmt.Sprintf("%s/users/%s/events", hostname, username)
+
+	header := map[string]string{
+		"Authorization": fmt.Sprintf("Bearer %s", config.GetGitHubToken()),
+	}
+
+	resp, err := c.requester.Get(url, header)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("error getting user events: %s", resp.Status)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading response body: %w", err)
+	}
+
+	var events []UserEvent
+	if err := json.Unmarshal(body, &events); err != nil {
+		return nil, fmt.Errorf("error unmarshalling response body: %w", err)
+	}
+
+	return events, nil
 }
